c79cbf12fc0f68d9a945dfba3b30c606a33f6e33
salman-ctrl <zirmanvictory@gmail.com>
2025-10-17 16:33:12 +0700

    first commit

---
+++ trafellowbe/app/Http/Controllers/EventParticipantsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\event_participants;
+use Illuminate\Http\Request;
+
+class EventParticipantsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(event_participants $event_participants)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(event_participants $event_participants)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, event_participants $event_participants)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(event_participants $event_participants)
+    {
+        //
+    }
+}
